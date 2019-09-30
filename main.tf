718125461402715d37cfd9e55bf7612f9e926260
Roger Chao <rogman@gmail.com>
2019-09-29 21:00:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,8 @@ module "ec2_instance" {
 
   ami = "ami-0932686c"
   instance_type = "t2.small"
-  name = "aws-instance-1"
+  name = "aws-module-instance-1"
+  subnet_ids = ["${data.terraform_remote_state.rogercorp_aws_vpc_prod.public_subnets}"]
   vpc_security_group_ids = ["${module.security_group.this_security_group_id}"]
 }
 
@@ -29,6 +30,6 @@ module "security_group" {
   source  = "app.terraform.io/rogercorp/security-group/aws"
   version = "2.17.0"
 
-  name = "security-group"
+  name = "security-group-1"
   vpc_id = "${data.terraform_remote_state.rogercorp_aws_vpc_prod.vpc_id}"
 }
