1640f0ce0bea3c6e04906b8b515c45e4486aa598
Roger Chao <rogman@gmail.com>
2020-02-12 18:11:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,30 +20,5 @@ module "web_server_sg" {
 
   ingress_cidr_blocks = ["${data.terraform_remote_state.rogercorp_aws_vpc_prod.vpc_cidr_block}"]
 }
-  
-output "this_security_group_id" {
-  description = "The ID of the security group"
-  value       = module.web_server_sg.this_security_group_id
-}
-
-output "this_security_group_vpc_id" {
-  description = "The VPC ID"
-  value       = module.web_server_sg.this_security_group_vpc_id
-}
-
-output "this_security_group_owner_id" {
-  description = "The owner ID"
-  value       = module.web_server_sg.this_security_group_owner_id
-}
-
-output "this_security_group_name" {
-  description = "The name of the security group"
-  value       = module.web_server_sg.this_security_group_name
-}
-
-output "this_security_group_description" {
-  description = "The description of the security group"
-  value       = module.web_server_sg.this_security_group_description
-}
 
 
