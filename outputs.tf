0b61abc0b804fd6fce16389135e52f560a936063
rogman <rogman@0.hsd1.ca.comcast.net>
2020-04-12 13:41:17 -0700

    fix output name

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "security_group_id" {
   value       = module.web_server_sg.this_security_group_id
 }
 
-output "security_group_id" {
+output "security_group_name" {
   value       = module.web_server_sg.this_security_group_name
 }
