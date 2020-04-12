0b8c04c41a81da0acaa2b9b87e02d9477ea1a7fc
rogman <rogman@0.hsd1.ca.comcast.net>
2020-04-12 13:40:09 -0700

    Add output security_group_name to outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "security_group_id" {
   value       = module.web_server_sg.this_security_group_id
 }
+
+output "security_group_id" {
+  value       = module.web_server_sg.this_security_group_name
+}
