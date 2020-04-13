f780e650152dae7446a49fe35b92a3e73a9a6576
Roger Chao <rogman@gmail.com>
2020-04-12 19:43:09 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,4 @@
 output "security_group_id" {
-  value       = module.web_server_sg.this_security_group_id
+  value = module.sg.security_group_id
 }
 
-output "security_group_name" {
-  value       = module.web_server_sg.this_security_group_name
-}
